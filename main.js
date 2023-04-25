0b77947d4ec91ccf7ea6e9fe7e3bc9e7a617f502
aosman <pianogreen0@gmail.com>
2023-04-25 14:55:04 +0100

    add lighting

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 import * as THREE from 'three'
-
+//* Scene
 const scene = new THREE.Scene()
-
+//* Create the Sphere
 const geometry = new THREE.SphereGeometry(3,64,64)
 const material = new THREE.MeshStandardMaterial({color:"00ff83"})
 const mesh = new THREE.Mesh(geometry,material)
 
 scene.add(mesh)
+//* Add Light
 
+
+//* Add Camera
 const camera = new THREE.PerspectiveCamera(45,800,600)
+camera.position.z = 20
 scene.add(camera)
 
 const canvas = document.getElementById('canvas')
+//* Render this to screen
 const renderer = new THREE.WebGLRenderer({canvas})
 
 renderer.setSize(800,600)
