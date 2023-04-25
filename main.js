60761a51497e0004ccf15b0083dbbdd8c6ebab9d
aosman <pianogreen0@gmail.com>
2023-04-25 14:18:29 +0100

    adding scene

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ import * as THREE from 'three'
 const scene = new THREE.Scene()
 
 const geometry = new THREE.SphereGeometry(3,64,64)
+const material = new THREE.MeshStandardMaterial({color:"00ff83"})
+const mesh = new THREE.Mesh(geometry,material)
+
+scene.add(mesh)
