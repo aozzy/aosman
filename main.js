848712f7ec16e52ae1cb9813317d0e418d6a285a
aosman <pianogreen0@gmail.com>
2023-04-29 12:40:11 +0100

    adding

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import './style.css'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-
+import { gsap } from 'gsap'
 
 
 
@@ -25,6 +25,7 @@ scene.add(mesh)
 
 //* Add Light
 const light = new THREE.PointLight(0xfffff,1,100)
+// const ambientLight = new THREE.AmbientLight(0xffffff);
 light.position.set(0,10,10) // change this values to show different angles of the sphere to be in view
 scene.add(light)
 
@@ -65,9 +66,31 @@ window.addEventListener('resize',()=>{
 })
 
 //* this function will make sure that its constally being animated
+
+
+
+const tl = gsap.timeline({defaults:{duration:1}})
+tl.fromTo(mesh.scale,{z:0,x:0,y:0},{z:1,x:1,y:1})
+tl.fromTo('h1',{y:'-100%'},{y:'0%'})
+
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  mesh.rotation.x += 0.05;
+  mesh.rotation.y += 0.075;
+  mesh.rotation.z += 0.05;
+  
+  catImage.rotation.y = 0.1;
+  catImage.rotation.x = 0.1;
+  
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
 const loop = () => {
  controls.update()
   renderer.render(scene,camera)
   window.requestAnimationFrame(loop)
 }
+document.body.onscroll = moveCamera;
 loop()
